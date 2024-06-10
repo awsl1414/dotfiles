fe0bace25015f5c2e9645fa210f93da65d0615ff
awsl1414 <3030994569@qq.com>
2024-06-10 20:13:19 +0800

    update

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
-cp -r ~/.zshrc .
-cp -r ~/.p10k.zsh .
-cp -r ~/.aria2 .
-cp -r ~/.config/alacritty ./.config
-cp -r ~/.config/zellij ./.config
+rm -rf .zshrc
+rm -rf .p10k.zsh
+rm -rf .aria2
+rm -rf .config/alacritty
+rm -rf .config/zellij
+
+cp -rf ~/.zshrc .
+cp -rf ~/.p10k.zsh .
+cp -rf ~/.aria2 .
+cp -rf ~/.config/alacritty ./.config
+cp -rf ~/.config/zellij ./.config
+
+echo "update complete!"
