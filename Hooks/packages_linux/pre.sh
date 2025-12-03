74895996b0f24177add108cdb0a212bd582dd3ba
awsl1414 <3030994569@qq.com>
2025-12-04 02:05:52 +0800

    feat: :sparkles: update

--- Hooks/packages_linux/pre.sh
+++ Hooks/packages_linux/pre.sh
@@ -7,22 +7,16 @@ else
     export IS_ARCHLINUX=false
 fi
 
-# 如果是archlinux，安装$HOME/aur.txt和$HOME/pacman.txt中的软件包
-if [[ $IS_ARCHLINUX == true ]]; then
-    # 安装pacman软件包
-    if [[ -f $HOME/Configs/package_linux/pacman.txt ]]; then
-        sudo pacman -Syu --needed - < $HOME/pacman.txt --noconfirm
-    fi
+packages="$HOME"/packages.txt
 
-    if [[ -f $HOME/Configs/package_linux/aur.txt ]]; then
+if [[ $IS_ARCHLINUX == true ]]; then
+    if [[ -L "$packages" || -f "$packages" ]]; then
         # 检查是否安装了paru
         if ! command -v paru &> /dev/null; then
             # 安装paru
             sudo pacman -S --needed paru --noconfirm
         fi
-
-        # 使用paru安装AUR软件包
-        paru -S --needed - < $HOME/aur.txt --noconfirm
+        paru -S --needed - < $packages --noconfirm
     fi
 fi
 
