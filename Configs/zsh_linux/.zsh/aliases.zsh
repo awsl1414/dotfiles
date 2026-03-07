9a9ac9810654936db6047d9221a417203cf98f39
awsl1414 <3030994569@qq.com>
2026-03-07 22:01:11 +0800

    feat: :sparkles: 更新 zsh 别名，添加 nvim 检查和别名设置

--- Configs/zsh_linux/.zsh/aliases.zsh
+++ Configs/zsh_linux/.zsh/aliases.zsh
@@ -2,3 +2,8 @@ alias zed="zeditor"
 alias reboot="sudo systemctl reboot"
 alias shutdown="sudo systemctl poweroff"
 alias poweroff="sudo systemctl poweroff"
+
+# 检查是否安装 nvim
+if command -v nvim >/dev/null 2>&1; then
+    alias vim="nvim"
+fi
