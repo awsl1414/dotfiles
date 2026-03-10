29f434523ad1d76dd6dfaef8c829984d0e09d546
awsl1414 <3030994569@qq.com>
2026-03-10 10:38:34 +0800

    feat: ✨ 更新 ghostty 配置

--- Configs/zsh_macos/.zsh/aliases.zsh
+++ Configs/zsh_macos/.zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias vim="nvim"
 alias subl="/Applications/'Sublime Text.app'/Contents/SharedSupport/bin/subl"
 alias python="python3"
-alias b="brew"
+alias claude-mem='bun "$HOME/.claude/plugins/cache/thedotmack/claude-mem/10.5.2/scripts/worker-service.cjs"'
