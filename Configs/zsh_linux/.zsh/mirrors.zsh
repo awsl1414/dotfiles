2e064291c7b854cc89428722f45aca751f33187f
awsl1414 <3030994569@qq.com>
2026-03-27 23:08:39 +0800

    feat: ✨ 更新 Node.js 镜像配置为北京外国语大学镜像

--- Configs/zsh_linux/.zsh/mirrors.zsh
+++ Configs/zsh_linux/.zsh/mirrors.zsh
@@ -1,7 +1,7 @@
-# Node.js (nvm) - 清华镜像
+# Node.js (nvm) - 北京外国语大学镜像
 export NVM_NODEJS_ORG_MIRROR="https://mirrors.bfsu.edu.cn/nodejs-release"
 
-# Node.js (fnm) - 清华镜像
+# Node.js (fnm) - 北京外国语大学镜像
 export FNM_NODE_DIST_MIRROR="https://mirrors.bfsu.edu.cn/nodejs-release"
 
 # Rust (rustup) - rsproxy 镜像
